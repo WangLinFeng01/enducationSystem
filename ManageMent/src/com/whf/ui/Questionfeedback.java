6973abbcaae69c1638b078b72325945417131fdd
xiaozhu123456 <70382137+xiaozhu123456@users.noreply.github.com>
2020-10-02 14:17:39 +0800

    朱珍西
    修改了疑难反馈窗口Questionfeedback，增加了弹窗提示和使用queryRunner的改版

--- ManageMent/src/com/whf/ui/Questionfeedback.java
+++ ManageMent/src/com/whf/ui/Questionfeedback.java
@@ -4,8 +4,12 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
+
+import com.whf.pojo.Student;
 import com.whf.util.JdbcUtils;
+import com.whf.util.QueryRunner;
 import com.whf.util.StringUtil;
 
 import javax.swing.JButton;
@@ -24,12 +28,13 @@ import java.util.List;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
 
-public class Questionfeedback {
-
-	protected static final Connection DbUtil = null;
+public class Questionfeedback extends JFrame{
+    
 	private JFrame frame;
 	private JTextField textField;
 	protected int x;
+	private JLabel lblNewLabel_1;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -70,35 +75,58 @@ public class Questionfeedback {
 		JButton btnNewButton = new JButton("\u63D0\u4EA4");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				String Fname1=textField_1.getText();
 				String Feedback=textField.getText();
-				 int x = 0;
+				if(StringUtil.isEmpty(Fname1)) {
+					JOptionPane.showMessageDialog(null, "姓名不能为空！");
+					JTextComponent FnameText = null;//JTextComponent 文本组件
+					FnameText.setText("");
+			       return;	
+				}
+				if(StringUtil.isEmpty(Feedback)) {
+					JOptionPane.showMessageDialog(null, "内容不能为空！");
+					JTextComponent FeedbackText = null;
+					FeedbackText.setText("");
+			       return;	
+				}
+				              
+					
+				   //数据导入数据库
+				   int x = 0;
 				   try {
-					Connection con =JdbcUtils.getConnection();
-					String sql = "insert into feedback values(DEFAULT,'"+Feedback+"');" ;
-					
-					
-					//拿到预编译
-					PreparedStatement ps = con.prepareStatement(sql);
-					boolean rs = ps.execute();
-						
+					   //这是框架的插入用法
+					String sql = "insert into feedback values(DEFAULT,'"+Fname1+"','"+Feedback+"');" ;
+					Object[] params= null;
+					new QueryRunner().execute(sql, params);
 				} catch (Exception e1) {
 					e1.printStackTrace();
-				}
+				}  finally {
+					JOptionPane.showMessageDialog(null, "反馈成功！");
+			   }
 				   return;
 			}
+			
 		});
-		btnNewButton.setBounds(246, 253, 122, 41);
+		btnNewButton.setBounds(238, 354, 122, 41);
 		frame.getContentPane().add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("\u8BF7\u8F93\u5165\u4F60\u7684\u7591\u95EE\u70B9\u6216\u53CD\u9988");
-		lblNewLabel.setBounds(64, 88, 58, 15);
+		JLabel lblNewLabel = new JLabel("\u53CD\u9988\u5185\u5BB9\uFF1A");
+		lblNewLabel.setBounds(62, 229, 65, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(132, 85, 344, 41);
+		textField.setBounds(137, 216, 344, 41);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
+		
+		lblNewLabel_1 = new JLabel("\u53CD\u9988\u4EBA\uFF1A");
+		lblNewLabel_1.setBounds(69, 114, 58, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(137, 101, 162, 41);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
 	}
 
 }
